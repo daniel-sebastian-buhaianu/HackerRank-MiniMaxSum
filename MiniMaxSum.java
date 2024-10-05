b9de637d635607ebe522810890d8bea96d6b8964
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2024-10-05 19:06:16 +0300

    Improve solution

--- MiniMaxSum.java
+++ MiniMaxSum.java
@@ -4,54 +4,48 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.function.*;
 import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'miniMaxSum' function below.
-     *
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
     public static void miniMaxSum(List<Integer> arr) {
-        long maxSum = Long.MIN_VALUE;
-        long minSum = Long.MAX_VALUE;
+        long arrSum = 0;
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
         
-        for (int i = 0, n = arr.size(); i < n; i++) {
-            long sum = 0;
+        for (int num : arr)
+        {
+            arrSum += num;
             
-            for (int j = 0; j < n; j++) {
-                if (j != i) {
-                    sum += arr.get(j);
-                }
+            if (num < minNum)
+            {
+                minNum = num;
             }
             
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-            
-            if (sum < minSum) {
-                minSum = sum;
+            if (num > maxNum)
+            {
+                maxNum = num;
             }
         }
         
-        System.out.print(minSum + " " + maxSum);
+        long minSum = arrSum - maxNum;
+        long maxSum = arrSum - minNum;
+        
+        System.out.println(minSum + " " + maxSum);
     }
-
 }
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
 
         Result.miniMaxSum(arr);
 
